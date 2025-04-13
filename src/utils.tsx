f121b8aa4f365cd45c1572dca3d23e7d45b3b30b
Harshvardhan Wadekar <harshvardhan.hudhp22@sinhgad.edu>
2025-04-13 11:59:31 +0530

    utils.tsx

--- src/utils.tsx
+++ src/utils.tsx
@@ -9,7 +9,6 @@ export const generatePDF = async () => {
     return;
   }
 
-<<<<<<< HEAD
   const canvas = await html2canvas(element, { scale: 2 });
   const imgData = canvas.toDataURL("image/png");
 
@@ -20,30 +19,3 @@ export const generatePDF = async () => {
   pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
   pdf.save("resume.pdf");
 };
-=======
-  const pdf = new jsPDF("p", "mm", "a4");
-  const pdfWidth = pdf.internal.pageSize.getWidth();
-  const pdfHeight = pdf.internal.pageSize.getHeight();
-
-  const canvas = await html2canvas(element, { scale: 2 });
-  const imgData = canvas.toDataURL("image/png");
-
-  let imgHeight = (canvas.height * pdfWidth) / canvas.width;
-  let heightLeft = imgHeight;
-  let position = 0;
-
-  // Add the first page
-  pdf.addImage(imgData, "PNG", 0, position, pdfWidth, imgHeight);
-  heightLeft -= pdfHeight;
-
-  // Add additional pages if content overflows
-  while (heightLeft > 0) {
-    position = heightLeft - imgHeight;
-    pdf.addPage();
-    pdf.addImage(imgData, "PNG", 0, position, pdfWidth, imgHeight);
-    heightLeft -= pdfHeight;
-  }
-
-  pdf.save("resume.pdf");
-};
->>>>>>> dff9988a354e1df9ebb0b0a7f3ba49e5f3820c56
