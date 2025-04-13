87c3c2b2c97118b68c52c943e1518aedccfdbe84
Harshvardhan Wadekar <harshvardhan.hudhp22@sinhgad.edu>
2025-04-13 11:09:54 +0530

    utils.tsx

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,20 +1,14 @@
 export const generatePDF = () => {
-  // mobiles
-  if ("ontouchstart" in document.documentElement) {
-    alert("WIP!");
-  } // for pcs
-  else {
-    const element = document.getElementById("resumeResult");
-    const stylesheet = document.styleSheets[0];
+  const element = document.getElementById("resumeResult");
+  const stylesheet = document.styleSheets[0];
 
-    // set @page's size = resume div's dimensions
-    stylesheet.insertRule(
-      `@page { size: ${element?.offsetWidth}px ${
-        element?.offsetHeight + 1
-      }px; }`,
-      stylesheet.cssRules.length,
-    );
+  // set @page's size = resume div's dimensions
+  stylesheet.insertRule(
+    `@page { size: ${element?.offsetWidth}px ${
+      element?.offsetHeight + 1
+    }px; }`,
+    stylesheet.cssRules.length,
+  );
 
-    window.print();
-  }
+  window.print();
 };
